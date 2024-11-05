48fc10e43520d508a3a5e0368fd076e68cd44e24
DavidDawnroad <cypoverhire@gmail.com>
2024-11-05 15:22:09 +0900

    Refactor: 컨트롤러 파일명 및 내용 변경

--- src/main/java/com/test/hike/mapper/MapAPIMapper.java
+++
@@ -1,9 +0,0 @@
-package com.test.hike.mapper;
-
-import com.test.hike.dto.MapAPISpotDTO;
-
-public interface MapAPIMapper {
-
-	int add(MapAPISpotDTO dto);
-
-}
---
+++ src/main/java/com/test/hike/mapper/MapCustomMapper.java
@@ -0,0 +1,9 @@
+package com.test.hike.mapper;
+
+import com.test.hike.dto.MapCustomDTO;
+
+public interface MapCustomMapper {
+
+	//int add(MapCustomDTO dto);
+
+}
