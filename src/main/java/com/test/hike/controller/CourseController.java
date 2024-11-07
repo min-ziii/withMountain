377ad0e04d9812d991ae740e7176997f95af589f
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2024-11-07 19:30:31 +0900

    Feat: 코스 산 선택 DB 연동

--- src/main/java/com/test/hike/controller/CourseController.java
+++ src/main/java/com/test/hike/controller/CourseController.java
@@ -1,15 +1,29 @@
 package com.test.hike.controller;
 
+import com.test.hike.dao.MapCustomDAO;
+import com.test.hike.dto.MountainDTO;
+import com.test.hike.mapper.MapCustomMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/course")
 public class CourseController {
+    private final MapCustomDAO dao;
 
     @GetMapping("")
-    public String search() {
+    public String search(Model model) {
+
+        List<MountainDTO> mountains = dao.getSearchMountainSpots();
+
+        model.addAttribute("mountains", mountains);
+
         return "course.search";
     }
 
--- src/main/java/com/test/hike/mapper/MapCustomMapper.java
+++ src/main/java/com/test/hike/mapper/MapCustomMapper.java
@@ -2,6 +2,7 @@ package com.test.hike.mapper;
 
 import java.util.List;
 
+import com.test.hike.dto.MountainDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.test.hike.dto.HikingRoadSpotDTO;
@@ -10,5 +11,7 @@ import com.test.hike.dto.HikingRoadSpotDTO;
 public interface MapCustomMapper {
 	
 	List<HikingRoadSpotDTO> getHikingRoadSpots(); // DB에서 등산로 지점 데이터 전부를 가져오는 method
+
+	List<MountainDTO> getSearchMountainSpots(); // 산 위치
 
 }
