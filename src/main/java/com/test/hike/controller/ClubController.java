736a35196a043f5fd1d89f49185e870b5e15ab84
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2024-11-08 12:12:47 +0900

    Merge: ClubController 충돌 병합

--- src/main/java/com/test/hike/controller/ClubController.java
+++ src/main/java/com/test/hike/controller/ClubController.java
@@ -12,43 +12,15 @@ public class ClubController {
 	public String index() {
 		return "club.index";
 	}
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
->>>>>>> Stashed changes
-	
-	@GetMapping("/view") 
-	public String view() {
-		return "club.view";
-	}
-	
-	@GetMapping("/template") 
-<<<<<<< Updated upstream
-=======
 
 	@GetMapping("/template")
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 	public String list() {
-=======
-
-
-	@GetMapping("/template")
-	public String template() {
->>>>>>> Stashed changes
 		return "club.template";
 	}
 	
 	@GetMapping("/add") 
 	public String add() {
 		return "club.add";
-	}
-	
-	@GetMapping("/view") 
-	public String view() {
-		return "club.view";
 	}
 
 	@GetMapping("/scheduler")
