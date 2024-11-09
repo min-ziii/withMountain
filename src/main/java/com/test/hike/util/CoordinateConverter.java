c8cc9046d8a8a9ac953ff9ba40ef48222cc59d53
Gwan-Woo-Jeong <gwanuuoo@gmail.com>
2024-11-09 10:50:00 +0900

    Refactor: 좌표변환 프로그램 테이블명 동적으로 변경

--- src/main/java/com/test/hike/util/CoordinateConverter.java
+++ src/main/java/com/test/hike/util/CoordinateConverter.java
@@ -1,25 +1,29 @@
 package com.test.hike.util;
 
-import org.locationtech.proj4j.CoordinateReferenceSystem;
-import org.locationtech.proj4j.CoordinateTransform;
-import org.locationtech.proj4j.ProjCoordinate;
-import org.locationtech.proj4j.CRSFactory;
-import org.locationtech.proj4j.CoordinateTransformFactory;
+import org.locationtech.proj4j.*;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CoordinateConverter {
+
+    private static final String SPOT = "HIKINGROADSPOT";
+    private static final String COORD = "HIKINGROADCOORD";
+
+    private static final Map<String, String[]> TABLE_COLUMN_MAP = new HashMap<>();
+
+    static {
+        TABLE_COLUMN_MAP.put(SPOT, new String[]{"SPOT_ID", "SPOT_X", "SPOT_Y"});
+        TABLE_COLUMN_MAP.put(COORD, new String[]{"COORD_ID", "ROAD_X", "ROAD_Y"});
+    }
 
     public static void main(String[] args) {
         String jdbcUrl = "jdbc:oracle:thin:@15.165.120.37:1521:xe"; // JDBC URL
         String username = "hike";
         String password = "java1234";
+        String tableName = SPOT; // 중요!!!! 변환할 테이블 이름 (SPOT 또는 COORD)
 
-        // 드라이버 로드
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
         } catch (ClassNotFoundException e) {
@@ -34,29 +38,35 @@ public class CoordinateConverter {
             // EPSG:4326 좌표계 정의
             String epsg4326Projection = "+proj=longlat +datum=WGS84 +no_defs";
 
-            // CRSFactory를 사용하여 좌표계 객체 생성
             CRSFactory crsFactory = new CRSFactory();
             CoordinateReferenceSystem tmCRS = crsFactory.createFromParameters("TM", tmProjection);
             CoordinateReferenceSystem epsg4326CRS = crsFactory.createFromParameters("EPSG:4326", epsg4326Projection);
 
-            // CoordinateTransformFactory를 사용하여 변환 객체 생성
             CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
             CoordinateTransform transform = ctFactory.createTransform(tmCRS, epsg4326CRS);
 
-            // 데이터 조회
-            String selectSQL = "SELECT COORD_ID, ROAD_X, ROAD_Y FROM HIKINGROADCOORD WHERE ROAD_X IS NOT NULL AND ROAD_Y IS NOT NULL";
+            String[] columns = TABLE_COLUMN_MAP.get(tableName);
+            if (columns == null || columns.length != 3) {
+                System.err.println("지원하지 않는 테이블 이름입니다: " + tableName);
+                return;
+            }
+            String idColumn = columns[0];
+            String xColumn = columns[1];
+            String yColumn = columns[2];
+
+            String selectSQL = String.format("SELECT %s, %s, %s FROM %s WHERE %s IS NOT NULL AND %s IS NOT NULL",
+                    idColumn, xColumn, yColumn, tableName, xColumn, yColumn);
             PreparedStatement selectStmt = connection.prepareStatement(selectSQL);
             ResultSet resultSet = selectStmt.executeQuery();
 
-            // 업데이트 쿼리 준비
-            String updateSQL = "UPDATE HIKINGROADCOORD SET ROAD_X = ?, ROAD_Y = ? WHERE COORD_ID = ?";
+            String updateSQL = String.format("UPDATE %s SET %s = ?, %s = ? WHERE %s = ?",
+                    tableName, xColumn, yColumn, idColumn);
             PreparedStatement updateStmt = connection.prepareStatement(updateSQL);
 
-            // 좌표 변환 및 업데이트
             while (resultSet.next()) {
-                int id = resultSet.getInt("COORD_ID");
-                double xValue = resultSet.getDouble("ROAD_X");
-                double yValue = resultSet.getDouble("ROAD_Y");
+                int id = resultSet.getInt(idColumn);
+                double xValue = resultSet.getDouble(xColumn);
+                double yValue = resultSet.getDouble(yColumn);
 
                 // 좌표 변환
                 ProjCoordinate tmCoord = new ProjCoordinate(xValue, yValue);
@@ -65,7 +75,6 @@ public class CoordinateConverter {
                 try {
                     transform.transform(tmCoord, epsg4326Coord);
 
-                    // 업데이트
                     updateStmt.setDouble(1, epsg4326Coord.x);
                     updateStmt.setDouble(2, epsg4326Coord.y);
                     updateStmt.setInt(3, id);
@@ -75,7 +84,6 @@ public class CoordinateConverter {
                 }
             }
 
-            // 리소스 정리
             resultSet.close();
             selectStmt.close();
             updateStmt.close();
